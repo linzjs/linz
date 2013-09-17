b75aa87f8cdcf7a4d82469f9ef6ee6bbd8ba811f
Scott Mebberson <scott@scottmebberson.com>
2013-09-17 15:13:08 +0930

    The modelList view now uses bootstrap.

--- routes/modelList.js
+++ routes/modelList.js
@@ -3,7 +3,10 @@ var path = require('path');
 /* GET /admin/models/list */
 var route = function (req, res) {
 
-	res.render(req.linz.views + '/modelList.jade', { models: req.linz.listModels() });
+	var models = req.linz.getModels();
+
+	res.render(req.linz.views + '/modelList.jade', { models: models });
+	console.log(models);
 
 };
 
