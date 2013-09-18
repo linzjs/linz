3c2909e5b6df7583da4dd774a4c7e69518f9b707
Scott Mebberson <scott@scottmebberson.com>
2013-09-18 11:43:27 +0930

    First version of a basic form output (you can't save as yet).

---
+++ middleware/recordEdit.js
@@ -0,0 +1,22 @@
+var util = require('util');
+
+module.exports = function () {
+
+	return function (req, res, next) {
+
+		req.linz.model = req.linz.models[req.params.model];
+
+		// simply return all docs
+		req.linz.model.findById(req.params.id, function (err, doc) {
+
+			if (!err) {
+				req.linz.record = doc;
+			}
+
+			next();
+
+		});
+
+	}
+
+}
---
+++ routes/recordEdit.js
@@ -0,0 +1,113 @@
+var forms = require('forms'),
+	fields = forms.fields,
+	validators = forms.validtors,
+	widgets = forms.widgets,
+	helpers = require('../lib/helpers-form');
+
+/* GET /admin/:model/:id/overview */
+var route = function (req, res) {
+
+	// create an object of fields we want to edit
+	var fds = {};
+	var m = req.linz.model;
+	var r = req.linz.record;
+
+	Object.keys(m.schema.paths).forEach(function (fieldName) {
+
+		if (fieldName !== '_id' && fieldName !== '__v') {
+
+			var field = m.schema.paths[fieldName];
+			var schemaType = helpers.findFieldType(field);
+
+			switch (schemaType) {
+
+				case 'String':
+				fds[field.path] = fields.string({
+									widget: widgets.text({
+										classes: ['form-control']
+									}),
+									value: r[fieldName],
+									cssClasses: {
+										label: ['col-lg-2 control-label']
+									}
+								});
+				break;
+
+				case 'Date':
+				fds[field.path] = fields.date({
+									widget: widgets.text({
+										classes: ['form-control']
+									}),
+									value: r[fieldName],
+									cssClasses: {
+										label: ['col-lg-2 control-label']
+									}
+								});
+				break;
+
+				case 'Text':
+				fds[field.path] = fields.string({
+									widget: widgets.textarea({
+										classes: ['form-control'],
+										rows: '6'
+									}),
+									value: r[fieldName],
+									cssClasses: {
+										label: ['col-lg-2 control-label']
+									}
+								});
+				break;
+
+				case 'Number':
+				fds[field.path] = fields.number({
+									widget: widgets.text({
+										classes: ['form-control']
+									}),
+									value: r[fieldName],
+									cssClasses: {
+										label: ['col-lg-2 control-label']
+									}
+								});
+				break;
+
+				case 'Oid':
+				fds[field.path] = fields.string({
+									widget: widgets.text({
+										classes: ['form-control']
+									}),
+									value: r[fieldName],
+									cssClasses: {
+										label: ['col-lg-2 control-label']
+									}
+								});
+				break;
+
+			}
+
+		}
+
+	});
+
+	var editForm = forms.create(fds);
+
+	res.render(req.linz.views + '/recordEdit.jade', {
+		model: req.linz.model,
+		record: req.linz.record,
+		form: editForm.toHTML(function (name, object) {
+			return bootstrapField(name, object);
+		})
+	});
+
+};
+
+function bootstrapField (name, object) {
+
+	var label = object.labelHTML(name);
+	var error = object.error ? '<p class="form-error-tooltip">' + object.error + '</p>' : '' ;
+	var widget = '<div class="col-lg-10">' + object.widget.toHTML(name, object) + error + '</div>';
+
+	return '<div class="form-group ' + (error !== '' ? 'has-error':'') + '">' + label + widget + '</div>';
+
+}
+
+module.exports = route;
