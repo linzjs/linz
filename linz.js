198c6fba28f449d94cd17820916dd7ea03124b9d
Scott Mebberson <scott@scottmebberson.com>
2013-11-06 11:39:11 +1030

    Extend types is now controlled and exposed via linz.formtools

--- linz.js
+++ linz.js
@@ -1,3 +1,4 @@
 
 module.exports.Linz = require('./lib/linz');
 module.exports.formtools = require('./lib/formtools/');
+module.exports.formtools.extendTypes = require('./lib/formtools/extend-types');
