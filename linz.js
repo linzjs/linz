9a3f77b9c1b04f3956adf9de65139763adbc9070
Scott Mebberson <scott@scottmebberson.com>
2013-11-12 08:30:34 +1030

    Added an array renderer and have expose them to other modules.

--- linz.js
+++ linz.js
@@ -2,3 +2,4 @@
 module.exports.Linz = require('./lib/linz');
 module.exports.formtools = require('./lib/formtools/');
 module.exports.formtools.extendTypes = require('./lib/formtools/extend-types');
+module.exports.formtools.cellRenderers = require('./lib/formtools/renderers-cell');
