f1f6cb806e44833ac494042ccb3a627c76bd2404
Scott Mebberson <scott@scottmebberson.com>
2013-11-13 08:09:03 +1030

    Started to make some structural changes to the way Linz hangs together, to provide more functionality.

--- linz.js
+++ linz.js
@@ -1,5 +1,28 @@
 
-module.exports.Linz = require('./lib/linz');
+var Linz = require('./lib/linz'),
+	application = require('./lib/application');
+
+exports = module.exports = createLinz;
+
+function createLinz() {
+
+	var linz = new Linz();
+	merge(linz, application);
+	linz.init();
+	return linz;
+
+}
+
+merge = function(a, b){
+	if (a && b) {
+		for (var key in b) {
+			a[key] = b[key];
+		}
+	}
+	return a;
+};
+
+// module.exports.Linz = require('./lib/linz');
 module.exports.formtools = require('./lib/formtools/');
 module.exports.formtools.extendTypes = require('./lib/formtools/extend-types');
 module.exports.formtools.cellRenderers = require('./lib/formtools/renderers-cell');
