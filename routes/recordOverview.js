7edd88b60d83fa12cd7644d92b32b6bd147e54dc
Scott Mebberson <scott@scottmebberson.com>
2014-01-06 14:52:23 +1030

    Upgraded Linz so that you can now provide custom record overviews (the body content anyway).

--- routes/recordOverview.js
+++ routes/recordOverview.js
@@ -3,9 +3,32 @@
 /* GET /admin/:model/:id/overview */
 var route = function (req, res) {
 
-	res.render(req.linz.views + '/recordOverview.jade', {
+	var locals = {
 		model: req.linz.model,
 		record: req.linz.record
+	};
+
+	async.series([
+
+		function (callback) {
+
+			if (!req.linz.model.overview || !req.linz.model.overview.renderer) {
+
+				return callback(null);
+
+			}
+
+			req.linz.model.overview.renderer.call(this, req.linz.record, req.linz.model, req.linz, function (err, content) {
+				locals.overviewBody = content;
+				callback(null);
+			});
+
+		}
+
+	], function (err, results) {
+
+		res.render(req.linz.views + '/recordOverview.jade', locals);
+
 	});
 
 };
