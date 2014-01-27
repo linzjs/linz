645475d94822b95cb3190de22ae42751d51cce9b
Scott Mebberson <scott@scottmebberson.com>
2014-01-27 20:51:40 +1030

    Some fairly large refactoring. Changed the requires caching mechanism, and linz now tries to load models automatically.

--- linz.js
+++ linz.js
@@ -1,23 +1,2 @@
 
-var Linz = require('./lib/linz'),
-	application = require('./lib/application'),
-	utils = require('./lib/utils');
-
-function createLinz(expressApp, models, options) {
-
-	if (!expressApp) throw new Error('You must pass an express app to Linz.');
-
-	models = models || {};
-	options = options || {};
-
-	var linz = new Linz(expressApp, models, options);
-	utils.merge(linz, application);
-	linz.init();
-	return linz;
-
-}
-
-exports = module.exports = createLinz;
-module.exports.formtools = require('./lib/formtools/');
-module.exports.formtools.extendTypes = require('./lib/formtools/extend-types');
-module.exports.formtools.cellRenderers = require('./lib/formtools/renderers-cell');
+module.exports = require('./lib/linz');
