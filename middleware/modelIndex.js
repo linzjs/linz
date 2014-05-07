e1de590b42efd8eea5a524857e56a7d48bdaadb4
Scott Mebberson <scott@scottmebberson.com>
2014-05-07 15:02:15 +0930

    Refactored the cell renderers quite a bit making them asynchronous (removed string), and added new test cases. A record is now updated with rendered cell values prior to pass off to the view (reducing view logic is good).

--- middleware/modelIndex.js
+++ middleware/modelIndex.js
@@ -5,18 +5,24 @@ module.exports = function (model) {
 
 	return function (req, res, next) {
 
+        var records = [];
+
+        // set the model on linz
 		req.linz.model = req.linz.get('models')[req.params.model];
 
-		async.parallel([
+		async.series([
 
 			// grab the grid object and append it to the model, i.e. req.linz.model.grid.columns
 			function (cb) {
 
 				req.linz.model.getGrid(function (err, grid) {
 
-					req.linz.model.grid = grid;
+                    if (!err) {
+                        req.linz.model.grid = grid;
+                    }
 
-					cb(null);
+					cb(err);
+
 				});
 
 			},
@@ -34,22 +40,69 @@ module.exports = function (model) {
 				} else if (req.query.sort) {
 
 					query.sort(req.query.sort);
-					
+
 				}
 
 				query.exec(function (err, docs) {
 
-					if (!err) req.linz.records = docs;
-					cb(null);
+					if (!err) {
+
+                        records = docs;
+                    }
+
+					cb(err);
 
 				});
 
-			}
+			},
 
-		], function () {
+            // create the values for the datagrids for each doc
+            function (cb) {
+
+                // loop through each record
+                async.each(Object.keys(records), function (index, recordDone) {
+
+                    // turn each mongoose record, into a plain javascript object
+                    // otherwise it won't accept a string in a field of type Date
+                    records[index] = records[index].toObject();
+
+                    // loop through each column
+                    async.each(Object.keys(req.linz.model.grid.columns), function (column, columnDone) {
+
+                        // call the cell renderer and update the content with the result
+                        req.linz.model.grid.columns[column].renderer(records[index][column], records[index], req.linz.model.modelName, function (err, value) {
+
+                            if (!err) {
+                                records[index][column] = value;
+                            }
+
+                            return columnDone(err);
+
+                        });
+
+
+                    }, function (err) {
+
+                        recordDone(err);
+
+                    });
+
+
+                }, function (err) {
+
+                    cb(err);
+
+                });
+
+            }
+
+		], function (err, result) {
+
+            // map the update records to linz
+            req.linz.records = records;
 
 			// next middleware
-			next();
+			next(err);
 
 		});
 
