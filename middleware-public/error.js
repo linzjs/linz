d8398180e1498615122be50a18d50f7a42889ecd
Sandy Trinh <sandicino@gmail.com>
2014-07-03 15:20:00 +0930

    Re-instated error logging to console.

--- middleware-public/error.js
+++ middleware-public/error.js
@@ -2,6 +2,8 @@ linz = require('../');
 
 module.exports = function (err, req, res, next) {
 
+    console.error(err.stack);
+
     res.render(linz.views + '/error.jade', {
         error: err,
         returnUrl: req.headers.referer
