22507c12428cad90cd87e016ec2294378fba4af7
Sandy Trinh <sandicino@gmail.com>
2014-07-04 09:17:42 +0930

    Added forgot your password feature which allows a user to reset a password without a login.

---
+++ middleware/forgottenPassword.js
@@ -0,0 +1,19 @@
+var linz = require('../');
+
+module.exports = {
+    get: function (req, res, next) {
+        return next();
+    },
+
+    post: function (req, res, next) {
+
+        if (!req.body.email) {
+            return next(new Error('An email address is required in order to reset your password.'));
+        }
+
+        var User = linz.get('models')[linz.get('user model')];
+
+        User.resetPassword(req.body.email, req, next);
+
+    }
+}
