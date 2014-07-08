8306cee9abf8d3adcc5900f11f05e3fcd81a246a
Scott Mebberson <scott@scottmebberson.com>
2014-07-08 11:07:57 +0930

    Embedded document defaults are now being applied, to config fields, before being saved.

--- routes/configSave.js
+++ routes/configSave.js
@@ -42,18 +42,47 @@ var route = function (req, res, next) {
 
                         if (formUtils.schemaType(req.linz.config.schema.paths[field]) === 'documentarray') {
 
-                            // turn the json into an object
-                            req.body[field] = JSON.parse(req.body[field]);
+                            // turn the json into an object, so that we can loop over it
+                            var documents = JSON.parse(req.body[field]);
+
+                            // clear the array as we'll push new documents in every time
+                            record[field] = [];
+
+                            // loop over each document, apply defaults and push into the array
+                            documents.forEach(function (doc) {
+
+                                // loop through each field in the embedded document schema
+                                req.linz.config.schema.paths[field].schema.eachPath(function (fieldName, field) {
+
+                                    // if the field hasn't come through the form, apply the default
+                                    if (doc[fieldName] === undefined) {
+                                        doc[fieldName] = linz.formtools.utils.getDefaultValue(field);
+                                    }
+
+                                });
+
+                                // push the new object (complete with defaults) into the array
+                                record[field].push(doc);
+
+                            });
+
+
+                        } else {
+
+                            // handle type conversion here
+                            if (form[field].type === 'array' && typeof req.body[field] === 'string') {
+                                req.body[field] = [req.body[field]];
+                            }
+
+                            // handle booleans
+                            if (form[field].type === 'boolean') {
+                                req.body[field] = utils.asBoolean(req.body[field]);
+                            }
+
+                            // go through the transform function if one exists
+                            record[field] = (form[field].transform) ? form[field].transform(req.body[field], 'beforeSave') : req.body[field];
 
                         }
-
-                        // handle type conversion here
-                        if (form[field].type === 'array' && typeof req.body[field] === 'string') {
-                            req.body[field] = [req.body[field]];
-                        }
-
-                        // go through the transform function if one exists
-                        record[field] = (form[field].transform) ? form[field].transform(req.body[field], 'beforeSave') : req.body[field];
 
                     }
 
@@ -62,7 +91,6 @@ var route = function (req, res, next) {
                 // update system required data
                 record.modifiedBy = req.user._id;
                 record.dateModified = new Date();
-
 
                 collection.update({ _id: req.params.config }, {$set: record}, {w:1}, function(err, result) {
 
