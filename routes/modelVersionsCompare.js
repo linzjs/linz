92314537d2eef5d6d457b9b5fd10deafb2917c59
Sandy Trinh <sandicino@gmail.com>
2014-09-04 11:28:57 +0930

    Added the ability to rollback to a selected previous version.

--- routes/modelVersionsCompare.js
+++ routes/modelVersionsCompare.js
@@ -5,6 +5,7 @@ module.exports = {
     get: function (req, res) {
 
         var locals = {
+                label: 'Compare versions',
                 latest: req.linz.history.latest,
                 previous: req.linz.history.previous,
                 diffs: req.linz.diffs
