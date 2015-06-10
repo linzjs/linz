36651c14b3305cf998223daf968b695b880aba5c
Scott Mebberson <scott@scottmebberson.com>
2015-06-10 21:18:42 +1000

    You can now provide a custom renderer for the admin login form.

--- routes/adminLogin.js
+++ routes/adminLogin.js
@@ -1,15 +1,44 @@
 var linz = require('../'),
-    path = require('path');
+    path = require('path'),
+    async = require('async');
 
 /* GET /admin/login */
 var route = {
 
-	get: function (req, res) {
+	get: function (req, res, next) {
 
         // check if resetPassword() is defined for user model
-        var hasResetPassword = linz.api.model.get(linz.get('user model')).sendPasswordResetEmail;
+        var locals = {
+            hasResetPassword: linz.api.model.get(linz.get('user model')).sendPasswordResetEmail
+        };
 
-		res.render(req.linz.views + '/adminLogin.jade', { hasResetPassword: hasResetPassword });
+        // pre-render the admin view, ready to plug into the larger wrapper
+        async.series([
+
+            function (done) {
+
+                // render either a custom form, or the default Linz form
+                (typeof linz.get('admin login view') === 'function') ? linz.get('admin login view').call(null, locals, done) : (function () {
+
+                    linz.app.render(path.resolve(req.linz.views, 'login', 'form.jade'), locals, done);
+
+                })();
+
+            }
+
+
+        ], function (err, html) {
+
+            if (err) {
+                return next(err);
+            }
+
+            // now render the final template, within the layout
+            return res.render(req.linz.views + '/adminLogin.jade', {
+                html: html
+            });
+
+        });
 
 	},
 
