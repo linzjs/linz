3240f072342b040af1c6ba87c2a17ee222fdb984
Sandy Trinh <sandicino@gmail.com>
2015-08-03 09:08:10 +0930

    Renamed disabled as disable for linz cache settings and tidy up the req.linz.cache namespace to allows invalidate cache settings for configs and models

--- middleware/linz.js
+++ middleware/linz.js
@@ -10,7 +10,10 @@ module.exports = function linzNamespace (req, res, next) {
     req.linz.cache.navigation = req.linz.cache.navigation || {};
     req.linz.cache.navigation.invalidate = req.linz.cache.navigation.invalidate || false;
     req.linz.cache.permissions = req.linz.cache.permissions || {};
-    req.linz.cache.permissions.invalidate = req.linz.cache.permissions.invalidate || false;
+    req.linz.cache.permissions.configs = req.linz.cache.permissions.configs || {};
+    req.linz.cache.permissions.configs.invalidate = req.linz.cache.permissions.configs.invalidate || false;
+    req.linz.cache.permissions.models = req.linz.cache.permissions.models || {};
+    req.linz.cache.permissions.models.invalidate = req.linz.cache.permissions.models.invalidate || false;
 
     return next();
 
