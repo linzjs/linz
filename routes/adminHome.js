092d1087ce79a7e0832705583b09e83f9a05cde0
Allan Chau <allan.chau@icloud.com>
2017-05-01 16:21:42 +0930

    Add admin home route

--- routes/adminHome.js
+++ routes/adminHome.js
@@ -1,10 +1,17 @@
-var linz = require('../');
+'use strict';
 
-/* GET /admin */
-var route = function (req, res) {
+const linz = require('../');
+const path = require('path');
 
-	res.redirect(307, linz.get('admin path') + '/models/list');
-
+/**
+ * GET /admin
+ * Admin route redirect.
+ * @param  {Object} req Express Request object.
+ * @param  {Object} res Express Response object.
+ * @return {Void}       Redirects to the admin home.
+ */
+const route = (req, res) => {
+	return res.redirect(307, path.join(linz.get('admin path'), linz.get('admin home')));
 };
 
 module.exports = route;
