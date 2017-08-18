cfe9cd3dfd1df5acf974297766012357fcfce7b5
Allan Chau <allan.chau@icloud.com>
2017-08-18 12:16:32 +0930

    Update search to accept an input type

--- public/js/search.js
+++ public/js/search.js
@@ -5,15 +5,40 @@
 $(function ready () {
 
     var modelSearch = $('.model-search');
+    var searchInput = modelSearch.find('.search');
+    var form = $('form.filters');
+    var searchFilter = form.find('[name="searchfilter"]');
+    var searchContent = form.find('[name="searchcontent"]');
 
     var getActiveFilterValue = function getActiveFilterValue () {
         return modelSearch.find('.active input').val();
     }
 
-    var searchInput = modelSearch.find('.search');
-    var form = $('form.filters');
-    var searchFilter = form.find('[name="searchfilter"]');
-    var searchContent = form.find('[name="searchcontent"]');
+    var getActiveFilterType = function getActiveFilterType () {
+        return modelSearch.find('option[value="' + getActiveFilterValue() + '"]').attr('data-filter-type');
+    }
+
+    var updateInputType = function updateInputType () {
+
+        if (getActiveFilterType()) {
+
+            searchInput.attr('type', getActiveFilterType());
+
+            return searchInput.hide().show(0);
+
+        }
+
+        searchInput.attr('type', 'text');
+
+        return searchInput.hide().show(0);
+
+    }
+
+    updateInputType();
+
+    modelSearch.find('.multiselect-container').on('change', function change () {
+        return updateInputType();
+    });
 
     searchInput.on('keypress', function keypress (event) {
 
