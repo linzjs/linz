531de940b8566262820f819f9b2c914084d3e6b1
Scott Mebberson <scott@scottmebberson.com>
2017-11-03 12:16:46 +1030

    Linz namespace in req can now be created before Linz does it.

--- middleware/namespace.js
+++ middleware/namespace.js
@@ -5,8 +5,8 @@ var linz = require('../');
 module.exports = function linzNamespace (req, res, next) {
 
     // namespace for all things Linz
-    req.linz = {};
-    req.linz.notifications = [];
+    req.linz = req.linz || {};
+    req.linz.notifications = req.linz.notifications || [];
     req.linz.cache = req.linz.cache || {};
     req.linz.cache.navigation = req.linz.cache.navigation || {};
     req.linz.cache.navigation.invalidate = req.linz.cache.navigation.invalidate || false;
