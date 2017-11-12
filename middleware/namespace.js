200a1931f75bb60484fb5156541350faaf9a18c7
Scott Mebberson <scott@scottmebberson.com>
2017-11-12 17:09:28 +1030

    Removed some code which I think is not being used.

--- middleware/namespace.js
+++ middleware/namespace.js
@@ -8,11 +8,6 @@ module.exports = function linzNamespace (req, res, next) {
     req.linz.cache = req.linz.cache || {};
     req.linz.cache.navigation = req.linz.cache.navigation || {};
     req.linz.cache.navigation.invalidate = req.linz.cache.navigation.invalidate || false;
-    req.linz.cache.permissions = req.linz.cache.permissions || {};
-    req.linz.cache.permissions.configs = req.linz.cache.permissions.configs || {};
-    req.linz.cache.permissions.configs.invalidate = req.linz.cache.permissions.configs.invalidate || false;
-    req.linz.cache.permissions.models = req.linz.cache.permissions.models || {};
-    req.linz.cache.permissions.models.invalidate = req.linz.cache.permissions.models.invalidate || false;
 
     return next();
 
