6b8a9ee56ced2a7d5dbea8365b53d14adfd36315
Allan Chau <allan.chau@icloud.com>
2017-12-14 12:31:13 +1030

    Update all findOne calls with the new plugin methods

--- routes/recordDelete.js
+++ routes/recordDelete.js
@@ -3,33 +3,34 @@ var linz = require('../');
 /* GET /admin/:model/:id/delete */
 var route = function (req, res, next) {
 
-	req.linz.model.findOne({_id: req.linz.record._id}, function (err, doc) {
+    req.linz.model.findOneDocument({ filter: { _id: req.linz.record._id } })
+        .exec(function (err, doc) {
 
-        if (err) {
-            return next(err);
-        }
-
-        // Skip to a 404 page.
-        if (!doc) {
-            return next();
-        }
-
-        if (!doc) {
-		  return res.redirect(linz.api.url.getAdminLink(req.linz.model));
-        }
-
-        doc.remove(function (removeErr) {
-
-            if (removeErr) {
-                return next(removeErr);
+            if (err) {
+                return next(err);
             }
 
+            // Skip to a 404 page.
+            if (!doc) {
+                return next();
+            }
+
+            if (!doc) {
             return res.redirect(linz.api.url.getAdminLink(req.linz.model));
+            }
+
+            doc.remove(function (removeErr) {
+
+                if (removeErr) {
+                    return next(removeErr);
+                }
+
+                return res.redirect(linz.api.url.getAdminLink(req.linz.model));
+
+            });
+
 
         });
-
-
-	});
 
 };
 
