7d72a1e5681bd1b43a68085246e1251b9987a840
Allan Chau <allan.chau@icloud.com>
2018-03-13 09:38:40 +1030

    Fix the count for index views

--- middleware/_modelIndex.js
+++ middleware/_modelIndex.js
@@ -283,6 +283,35 @@ module.exports = function  (req, res, next) {
 
             },
 
+            // minimise the fields we're selecting
+            function (cb) {
+
+                let fields = Object.keys(req.linz.model.list.fields);
+
+                // Work in the title field
+                linz.api.model.titleField(req.params.model, 'title', (err, titleField) => {
+
+                    if (err) {
+                        return cb(err);
+                    }
+
+                    fields.push(titleField);
+
+                    const select = fields.join(' ');
+
+                    query.select(select);
+
+                    // If they've provided the `listQuery` static, use it to allow customisation of the fields we'll retrieve.
+                    if (!req.linz.model.listQuery) {
+                        return cb();
+                    }
+
+                    req.linz.model.listQuery(req, query, cb);
+
+                });
+
+            },
+
             // get page total
             function (cb) {
 
@@ -312,35 +341,6 @@ module.exports = function  (req, res, next) {
 
                     });
 
-
-                });
-
-            },
-
-            // minimise the fields we're selecting
-            function (cb) {
-
-                let fields = Object.keys(req.linz.model.list.fields);
-
-                // Work in the title field
-                linz.api.model.titleField(req.params.model, 'title', (err, titleField) => {
-
-                    if (err) {
-                        return cb(err);
-                    }
-
-                    fields.push(titleField);
-
-                    const select = fields.join(' ');
-
-                    query.select(select);
-
-                    // If they've provided the `listQuery` static, use it to allow customisation of the fields we'll retrieve.
-                    if (!req.linz.model.listQuery) {
-                        return cb();
-                    }
-
-                    req.linz.model.listQuery(req, query, cb);
 
                 });
 
