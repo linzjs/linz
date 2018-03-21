4b2988dfeab95bbdfd59741b01015a75753a9ed4
Allan Chau <allan.chau@icloud.com>
2018-03-21 13:44:39 +1030

    Cache bust the model/index file
    Signed-off-by: Allan Chau <allan.chau@icloud.com>

--- routes/modelIndex.js
+++ routes/modelIndex.js
@@ -15,7 +15,7 @@ var route = function (req, res, next) {
                 src: `${linz.get('admin path')}/public/js/template.polyfill.js`,
             },
             {
-                src: `${linz.get('admin path')}/public/js/model/index.js?v4`,
+                src: `${linz.get('admin path')}/public/js/model/index.js?v5`,
             },
         ]),
         linz.api.views.getStyles(req, res),
