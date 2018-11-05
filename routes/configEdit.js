4387c4aea403e2d0a860fa03d61f35eca2315b4e
Allan Chau <allan.chau@icloud.com>
2018-11-05 11:40:04 +1030

    Revert config changes

--- routes/configEdit.js
+++ routes/configEdit.js
@@ -5,46 +5,46 @@ const linz = require('../');
 /* GET /admin/config/:config/overview */
 var route = function (req, res, next) {
 
-    let editForm = {};
+    linz.formtools.form.generateFormFromModel(req.linz.config.schema, req.linz.config.linz.formtools.form, req.linz.record, 'edit', function (err, editForm) {
 
-    linz.api.model.generateForm(req.linz.model, {
-        record: req.linz.record,
-        type: 'edit',
-    })
-        .then((form) => {
+        if (err) {
+            return next(err);
+        }
 
-            editForm = form;
+        Promise.all([
+            linz.api.views.getScripts(req, res, [
+                {
+                    src: '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.10/handlebars.min.js',
+                    integrity: 'sha256-0JaDbGZRXlzkFbV8Xi8ZhH/zZ6QQM0Y3dCkYZ7JYq34=',
+                    crossorigin: 'anonymous',
+                },
+                {
+                    src: `${linz.get('admin path')}/public/js/jquery.binddata.js`,
+                },
+                {
+                    src: `${linz.get('admin path')}/public/js/documentarray.js?v1`,
+                },
+                {
+                    src: `${linz.get('admin path')}/public/js/model/edit.js`,
+                },
+            ]),
+            linz.api.views.getStyles(req, res),
+        ])
+            .then(([scripts, styles]) => {
 
-            return Promise.all([
-                linz.api.views.getScripts(req, res, [
-                    {
-                        src: '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.10/handlebars.min.js',
-                        integrity: 'sha256-0JaDbGZRXlzkFbV8Xi8ZhH/zZ6QQM0Y3dCkYZ7JYq34=',
-                        crossorigin: 'anonymous',
-                    },
-                    {
-                        src: `${linz.get('admin path')}/public/js/jquery.binddata.js`,
-                    },
-                    {
-                        src: `${linz.get('admin path')}/public/js/documentarray.js?v2`,
-                    },
-                    {
-                        src: `${linz.get('admin path')}/public/js/model/edit.js`,
-                    },
-                ]),
-                linz.api.views.getStyles(req, res),
-            ]);
+                return res.render(linz.api.views.viewPath('configEdit.jade'), {
+                    actionUrl: linz.api.url.getAdminLink(req.linz.config, 'save', req.linz.record._id),
+                    cancelUrl: linz.api.url.getAdminLink(req.linz.config, 'list'),
+                    form: editForm.render(),
+                    record: req.linz.record,
+                    scripts,
+                    styles,
+                });
 
-        })
-        .then(([scripts, styles]) => res.render(linz.api.views.viewPath('configEdit.jade'), {
-            actionUrl: linz.api.url.getAdminLink(req.linz.config, 'save', req.linz.record._id),
-            cancelUrl: linz.api.url.getAdminLink(req.linz.config, 'list'),
-            form: editForm.render(),
-            record: req.linz.record,
-            scripts,
-            styles,
-        }))
-        .catch(next);
+            })
+            .catch(next);
+
+    });
 
 };
 
