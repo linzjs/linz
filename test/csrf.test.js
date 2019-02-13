e02e98dd3606b7207c2389939254eb014701ac6a
Allan Chau <allan.chau@icloud.com>
2019-02-13 13:47:56 +1030

    add tests for csrf protection

--- test/csrf.test.js
+++ test/csrf.test.js
@@ -7,6 +7,7 @@ beforeAll((done) => {
 
     linz.init({
         options: {
+            'csrf token': { value: 'token' },
             'mongo': 'mongodb://mongodb:27017/csrf',
             'user model': 'user',
             'load models': false,
@@ -22,6 +23,12 @@ beforeAll((done) => {
         });
 
         userSchema.plugin(linz.formtools.plugins.document, { model: { title: 'username' } });
+
+        userSchema.virtual('hasAdminAccess').get(() => true);
+
+        userSchema.methods.verifyPassword = function (candidatePassword, callback) {
+            return callback(null, this.password === candidatePassword);
+        };
 
         // Set manually as we're not loading via file.
         linz.set('models', { user: linz.mongoose.model('user', userSchema) });
@@ -59,5 +66,33 @@ test('provides csrf protection', async () => {
     expect(response.status).toBe(403);
     expect(response.text).toMatch(/ForbiddenError:\ invalid\ csrf\ token/);
 
+
+});
+
+test('allows valid requests through', async () => {
+
+    expect.assertions(2);
+
+    const request = supertest.agent(linz.app);
+    const user = new linz.api.model.get('user')();
+
+    user.set({
+        password: 'password',
+        username: 'test',
+    });
+
+    await user.save();
+
+    const token = await request.get('/admin/login').send();
+    const [csrfToken] = /(?<=name="_csrf" value=")(.*)(?="\/><div)/.exec(token.text);
+
+    const response = await request.post('/admin/login').send({
+        _csrf: csrfToken,
+        password: 'password',
+        username: 'test',
+    });
+
+    expect(response.status).toBe(302);
+    expect(response.text).toMatch(/Found.\ Redirecting\ to\ \/admin/);
 
 });
