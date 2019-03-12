05d78af5b8457df108a7db01325f933e5f180179
Scott Mebberson <scott@scottmebberson.com>
2019-03-12 13:16:08 +1030

    Disable any form post until the client-side CSRF code has executed.

--- public/js/csrf-submit.js
+++ public/js/csrf-submit.js
@@ -1,8 +1,14 @@
 'use strict';
 
-var insertCSRFToken = function () {
+var insertCSRFToken = function (form) {
 
-    $('form').each(function() {
+    if (!form) {
+        return;
+    }
+
+    $(form).each(function() {
+
+        // Now enable anything with data-linz-csrf-required, within form
 
         var tokenInput = document.createElement('input');
 
@@ -16,18 +22,57 @@ var insertCSRFToken = function () {
 
         $(this).prepend(tokenInput);
 
+        $('[data-linz-csrf-required]', this).removeClass('disabled');
+        $('[data-linz-csrf-required]', this).removeAttr('disabled');
+
     });
+
 
 };
 
-linz.addLoadEvent(function () {
+var findAForm = function (node) {
 
-    insertCSRFToken();
+    if (node.nodeType === Node.ELEMENT_NODE && node.tagName.toLowerCase() === 'form') {
+        insertCSRFToken(node);
+    }
 
-    // Update whenever a new form is loaded
-    $('.modal').on({
-        'show.bs.modal': insertCSRFToken,
-        'shown.bs.modal': insertCSRFToken
+    if (node.children) {
+
+        for (var child of node.children) {
+            findAForm(child);
+        }
+
+    }
+
+  }
+
+var mutated = function (mutationsList, observer) {
+
+    for (var mutation of mutationsList) {
+
+        if (mutation.type === 'childList' && mutation.addedNodes.length && !mutation.removedNodes.length) {
+
+            for (var node of mutation.addedNodes) {
+
+                findAForm(node);
+
+            }
+
+        }
+
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var mutationObserver = new MutationObserver(mutated);
+
+    mutationObserver.observe(document.body, {
+        childList: true,
+        subtree: true,
     });
+
+    findAForm(document.body);
 
 });
