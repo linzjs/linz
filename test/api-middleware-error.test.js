571d2b0bfdd2aed0938e7626f9d4ccac98f98fce
Scott Mebberson <scott@scottmebberson.com>
2019-04-12 11:25:14 +0930

    Improved the tests.

--- test/api-middleware-error.test.js
+++ test/api-middleware-error.test.js
@@ -1,19 +1,29 @@
 'use strict';
 
-const error = require('linz/lib/api/middleware/error');
+const { json, store } = require('linz/lib/api/error');
 
 test('stores the error on req', () => {
 
     const err = new Error('Test error');
     const req = {};
-    const res = {
-        status: jest.fn(code => res),
-        send: jest.fn(),
-    };
 
-    error(err, req, res);
+    store(err, req);
 
     expect(req.linz).toBeTruthy();
     expect(req.linz.error.message).toBe(err.message);
+
+});
+
+test('sets json and statusCode properties on an error', () => {
+
+    const err = new Error('Test error');
+
+    json(err);
+
+    expect(err.json).toBeDefined();
+    expect(err.json).toBeTruthy();
+
+    expect(err.statusCode).toBeDefined();
+    expect(err.statusCode).toBe(500);
 
 });
