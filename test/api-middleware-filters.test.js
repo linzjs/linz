18d001d1d13794e2b6d3f619ebac4cfe74468cf5
Allan Chau <allan.chau@icloud.com>
2019-06-07 08:39:52 +0930

    increase test coverage

--- test/api-middleware-filters.test.js
+++ test/api-middleware-filters.test.js
@@ -43,6 +43,9 @@ beforeAll((done) => {
                     email: true,
                     username: true,
                 },
+                filters: {
+                    title: { filter: linz.formtools.filters.text },
+                },
                 sortBy: ['username', 'email'],
             },
             model: {
@@ -131,6 +134,26 @@ test('selectedIds filter', async () => {
 
 });
 
+test('linz filters', async () => {
+
+    expect.assertions(1);
+
+    const result = await getFilters({
+        body: {
+            '_csrf': 'jax8OKTT-kXltsUkA1GB6iouHWkeYZacoMGc',
+            'filters': '{\"_csrf\":\"hohQlFpz-NlXdO1Cu5i8PV1glaQSCnylgAjI\",\"search\":\"\",\"page\":\"1\",\"pageSize\":\"20\",\"selectedFilters\":\"title\",\"sort\":\"title\",\"title\":[\"123\"]}',
+            'selectedFields': '',
+            'selectedIds': '',
+            'submit': ''
+        },
+        linz: { model: linz.api.model.get('user') },
+        params: { model: 'user' },
+    });
+
+    expect(result).toEqual({ "$and": [{ "$or": [{ "title": { "$regex": /123/gi } }] }] });
+
+});
+
 test('all filters', async () => {
 
     expect.assertions(1);
@@ -138,7 +161,7 @@ test('all filters', async () => {
     const result = await getFilters({
         body: {
             '_csrf': 'jax8OKTT-kXltsUkA1GB6iouHWkeYZacoMGc',
-            'filters': '{\"_csrf\":\"hohQlFpz-NlXdO1Cu5i8PV1glaQSCnylgAjI\",\"search\":\"test\",\"page\":\"1\",\"pageSize\":\"20\",\"selectedFilters\":\"\",\"sort\":\"label\"}',
+            'filters': '{\"_csrf\":\"hohQlFpz-NlXdO1Cu5i8PV1glaQSCnylgAjI\",\"search\":\"test\",\"page\":\"1\",\"pageSize\":\"20\",\"selectedFilters\":\"title\",\"sort\":\"title\",\"title\":[\"123\"]}',
             'selectedFields': '',
             'selectedIds': '548faa6c68acee0100799c8e',
             'submit': ''
@@ -147,6 +170,6 @@ test('all filters', async () => {
         params: { model: 'user' },
     });
 
-    expect(result).toEqual({ "$and": [{ "$or": [{ "username": { "$regex": /test/gi } }] }, { "_id": { "$in": [new linz.mongoose.Types.ObjectId('548faa6c68acee0100799c8e')] } }] });
+    expect(result).toEqual({ "$and": [{ "$or": [{ "title": { "$regex": /123/gi } }] }, { "$or": [{ "username": { "$regex": /test/gi } }] }, { "_id": { "$in": [new linz.mongoose.Types.ObjectId('548faa6c68acee0100799c8e')] } }] });
 
 });
