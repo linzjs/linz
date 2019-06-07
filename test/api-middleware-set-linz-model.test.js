847414a8bf4063a75f40b4308fa52d5bc0cf8399
Allan Chau <allan.chau@icloud.com>
2019-06-07 14:10:51 +0930

    guess the model if params is undefined

--- test/api-middleware-set-linz-model.test.js
+++ test/api-middleware-set-linz-model.test.js
@@ -10,7 +10,40 @@ jest.mock('linz', () => ({
     },
 }));
 
-const setLinzModel = require('linz/lib/api/middleware/set-linz-model');
+const {
+    getModelFromRequest,
+    setLinzModel,
+} = require('linz/lib/api/middleware/set-linz-model');
+
+test('getModelFromRequest', () => {
+
+    const paramModel = getModelFromRequest({
+        params: {
+            config: 'configModel',
+            model: 'paramModel',
+        },
+        path: '/model/pathModel/action/test',
+    });
+
+    const configModel = getModelFromRequest({
+        params: { config: 'configModel' },
+        path: '/model/pathModel/action/test',
+    });
+
+    const pathModel = getModelFromRequest({ path: '/model/pathModel/action/test' });
+
+    const nullModel = getModelFromRequest({ path: '/notamodelpath' });
+    const nullModel2 = getModelFromRequest({ path: '/models/list' });
+    const nullModel3 = getModelFromRequest({ path: '/modeltest' });
+
+    expect(paramModel).toBe('paramModel');
+    expect(configModel).toBe('configModel');
+    expect(pathModel).toBe('pathModel');
+    expect(nullModel).toBeNull();
+    expect(nullModel2).toBeNull();
+    expect(nullModel3).toBeNull();
+
+});
 
 test('it sets the linz model', () => {
 
