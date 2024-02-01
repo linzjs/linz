2e68fdff926f87e8dce1106c143524c34e442d72
Allan Chau <allan.chau@icloud.com>
2024-02-01 11:05:15 +0930

    mongoose v5 compatibility

--- app/models/mtUser/index.js
+++ app/models/mtUser/index.js
@@ -9,26 +9,27 @@ mtUserSchema.plugin(linz.formtools.plugins.document, require('./formtools'));
 // Add versions plugin
 mtUserSchema.plugin(linz.versions.plugin, require('./versions'));
 
-mtUserSchema.plugin(linz.concurrencyControl.plugin, require('./concurrency-control'));
+mtUserSchema.plugin(
+    linz.concurrencyControl.plugin,
+    require('./concurrency-control')
+);
 
-mtUserSchema.virtual('hasAdminAccess').get(function () {
+mtUserSchema.virtual('hasAdminAccess').get(function() {
     return this.bAdmin === true;
 });
 
-mtUserSchema.methods.verifyPassword = function (candidatePassword, callback) {
+mtUserSchema.methods.verifyPassword = function(candidatePassword, callback) {
     return callback(null, this.password === candidatePassword);
-}
+};
 
-mtUserSchema.pre('save', function (next, callback, req) {
-
-    if (req && req.user) {
-        this.modifiedBy = req.user.username;
+mtUserSchema.pre('save', function() {
+    if (this.user) {
+        this.modifiedBy = this.username;
     }
 
     this.increment();
 
-    return next(callback, req);
-
+    return next();
 });
 
 module.exports = linz.mongoose.model('mtUser', mtUserSchema);
--- routes/recordSave.js
+++ routes/recordSave.js
@@ -5,31 +5,33 @@ const modelHelpers = require('../lib/formtools/model');
 
 /* GET /admin/:model/:id/overview */
 const route = (req, res, next) => {
-
     const { model } = req.linz;
     const { form } = model;
 
     const data = modelHelpers.clean(req.body, req.linz.model);
 
-    linz.api.formtools.parseForm(model, req, form, {
-        data,
-        formType: 'edit',
-    })
-        .then((record) => {
-
-            return req.linz.model.findById(req.params.id).exec()
-                .then((doc) => {
-
-                    doc.set(record);
-
-                    return doc.save(req);
-
-                });
-
+    linz.api.formtools
+        .parseForm(model, req, form, {
+            data,
+            formType: 'edit',
         })
-        .then(doc => res.redirect(linz.api.url.getAdminLink(req.linz.model, 'overview', doc._id)))
-        .catch(next);
+        .then((record) => {
+            return req.linz.model
+                .findById(req.params.id)
+                .exec()
+                .then((doc) => {
+                    doc.set(record);
+                    doc.user = req.user;
 
+                    return doc.save();
+                });
+        })
+        .then((doc) =>
+            res.redirect(
+                linz.api.url.getAdminLink(req.linz.model, 'overview', doc._id)
+            )
+        )
+        .catch(next);
 };
 
 module.exports = route;
