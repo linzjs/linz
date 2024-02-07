b2f9f6a208e06b9ff481bdbd831925c1ed57ff84
Allan Chau <allan.chau@icloud.com>
2024-02-07 09:34:57 +0930

    fix up mongo connection

--- devops/infrastructure-mongo.sh
+++ devops/infrastructure-mongo.sh
@@ -13,10 +13,10 @@ fi
 if [[ $TARGET_ENV == "test" ]]; then
 
     # When in test mode, always run with a fresh database.
-    docker run --rm --name $NAME --network $NETWORK_NAME -d mongo:7.0 mongod --smallfiles --nojournal
+    docker run --rm --name $NAME --network $NETWORK_NAME -d mongo:7.0 mongod
 
 else
 
-    docker run --rm --name $NAME --network $NETWORK_NAME -v $PWD/devops/data/db:/data/db -d mongo:7.0 mongod --smallfiles --nojournal
+    docker run --rm --name $NAME --network $NETWORK_NAME -v $PWD/devops/data/db:/data/db -d mongo:7.0 mongod
 
 fi
